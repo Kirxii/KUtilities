da7614aeeb70ddc8f420d36a4c898f54db4d8fa1
Kirxii <131458787+Kirxii@users.noreply.github.com>
2024-08-23 18:46:37 +0700

    Third commit

--- lua/KUtilities/init.lua
+++ lua/KUtilities/init.lua
@@ -7,7 +7,14 @@ M.get_color = function(highlights, attributes)
 	assert(type(attributes) == "string" or type(attributes) == "table")
 
 	-- Converting into a table if hightlights is a singular string
-	local hls = type(highlights) == "string" and { highlights } or highlights
+	local hls = type(highlights) == "string" and { highlights } or highlights --[[@as table]]
+
+	for i, hl in pairs(hls) do
+		local result = vim.api.nvim_get_hl(0, { name = hl, link = false })
+		if type(result) == "table" and type(result[attributes]) == "number" then
+			return "#" .. string.format("#%06x", result[attributes])
+		end
+	end
 
 	return hls
 end
